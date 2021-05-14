a23165150dfb6a24a95e3a7e3607ff7833510147
unknown <jimpidas>
2021-05-14 19:36:25 +0530

    [Jyosmita]:Added the coffee maker test project

---
+++ CoffeMakerDemo/StarbuckStor.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoffeMakerDemo
+{
+    public class StarbuckStor
+    {
+
+        private readonly IMakeACoffee service;
+
+
+        //readonly const
+
+        public StarbuckStor(IMakeACoffee service)
+        {
+            this.service = service;
+        }
+
+        public string OrderCoffee(int sugarPerSpoon, int coffeeCount)
+        {
+            if (service.CheckIngredientAvailability())
+            {
+                return service.CoffeeMaking(sugarPerSpoon, coffeeCount);
+            }
+            else
+            {
+                return "Sorry! Coffee is not available.";
+            }
+
+        }
+    }
+}
